bc87f92dc12b00921e3226e0e4b4ee93f0261cd5
Yusuke Matsuoka <matsuokayusuke@matsuokayuusukes-MacBook-Pro.local>
2019-04-19 17:46:50 -0500

    first commit

---
+++ QRGeneratorPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRGeneratorPractice
+//
+//  Created by 松岡裕介 on 2019/04/19.
+//  Copyright © 2019 松岡裕介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
